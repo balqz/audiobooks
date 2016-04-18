fbc439996aed36dd718fd721bc31e0b2fd86312b
Gentra Aditya <gentra.a@gmail.com>
2016-04-18 09:29:41 +0700

    [CHORE] Added dynamic hidden field for "audio_file_url"

--- app/AudioBookChapter.php
+++ app/AudioBookChapter.php
@@ -1,5 +1,6 @@
 <?php namespace App;
 
+use App\Utils\AuthUtils;
 use Illuminate\Database\Eloquent\Model;
 
 class AudioBookChapter extends Model
@@ -10,6 +11,16 @@ class AudioBookChapter extends Model
     protected $fillable = [];
 
     protected $dates = ['created_at', 'updated_at', 'deleted_at'];
+
+    public function setAttributeVisibility()
+    {
+        $user = AuthUtils::authenticate();
+        if ($user == NULL || $this->user()->find($user->id) == NULL) {
+            $this->setHidden(['audio_file_url']);
+        } else {
+            $this->setHidden([]);
+        }
+    }
 
     public function audiobook()
     {
@@ -23,7 +34,17 @@ class AudioBookChapter extends Model
 
     public function user()
     {
-        return $this->belongsToMany('App\User', 'purchase', 'audiobookChapter_id');
+        return $this->belongsToMany('App\User', 'purchase', 'audiobookChapter_id', 'user_id');
+    }
+
+    public function toJson($options = 0){
+        $this->setAttributeVisibility();
+        return parent::toJson();
+    }
+
+    public function toArray(){
+        $this->setAttributeVisibility();
+        return parent::toArray();
     }
 
 }
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -22,9 +22,10 @@ class DatabaseSeeder extends Seeder
 
         factory(App\Category::class, 10)->create()->each(function ($c) {
             $c->audiobook()->saveMany(factory(App\AudioBook::class, 10)->create()->each(function ($d) {
-                $d->audiobookChapter()->saveMany(factory(App\AudioBookChapter::class, 10)->make()->each(function ($chapter) use ($d) {
-                    $chapter->purchase()->saveMany(factory(App\Purchase::class, 10)->create()->each(function ($purchase) use ($d) {
+                $d->audiobookChapter()->saveMany(factory(App\AudioBookChapter::class, 10)->create()->each(function ($chapter) use ($d) {
+                    $chapter->purchase()->saveMany(factory(App\Purchase::class, 10)->create()->each(function ($purchase) use ($d, $chapter) {
                         $purchase->user_id = User::all()->random(1)->id;
+                        $purchase->audiobookChapter_id = $chapter->id;
                         $purchase->audiobook_id = $d->id;
                     }));
 
